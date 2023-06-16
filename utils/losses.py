0afdea1fd4c32d015181d9e2ddb5754b6e9c95b8
leonl42 <noinnick8@gmail.com>
2023-06-16 18:48:46 +0200

    Implemented part 1

--- utils/losses.py
+++ utils/losses.py
@@ -2,39 +2,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from tqdm import tqdm
 
 def content_gatyes(f,f_target):
     """
     Computes the content loss proposed by Gatyes et al. (2016)
     Args:
-        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, width, height, channel)
-        - f_target <torch.Tensor>: feature tensor of the content image with shape (batch_size, width, height, channel)
+        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, channels, width, height)
+        - f_target <torch.Tensor>: feature tensor of the style image with shape (batch_size, channels, width, height)
     """
 
-    # reshape the feature maps into shape (batch_size x M_l x channels), where M_l is height*width of our feature map
-    f, f_target = torch.flatten(f,start_dim=1,end_dim=2), torch.flatten(f_target,start_dim=1,end_dim=2)
+    #f, f_target = torch.exp(f)/torch.exp(f).sum((1,2,3),keepdim=True), torch.exp(f_target)/torch.exp(f_target).sum((1,2,3),keepdim=True)
 
-    return F.mse_loss(f, f_target)
+    # reshape the feature maps into shape (batch_size x channels x M_l ), where M_l is height*width of our feature map
+    f, f_target = torch.flatten(f,start_dim=2,end_dim=3), torch.flatten(f_target,start_dim=2,end_dim=3)
+
+    return ((f - f_target)**2).mean((1,2))
 
 def style_gatyes(f, f_target):
     """
     Computes the style loss proposed by Gatyes et al. (2016)
     Args:
-        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, width, height, channel)
-        - f_target <torch.Tensor>: feature tensor of the style image with shape (batch_size, width, height, channel)
+        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, channels, width, height)
+        - f_target <torch.Tensor>: feature tensor of the style image with shape (batch_size, channels, width, height)
     """
 
-    # reshape the feature maps into shape (batch_size x M_l x channels), where M_l is height*width of our feature map
-    f, f_target = torch.flatten(f,start_dim=1,end_dim=2), torch.flatten(f_target,start_dim=1,end_dim=2)
+    #f, f_target = torch.exp(f)/torch.exp(f).sum((1,2,3),keepdim=True), torch.exp(f_target)/torch.exp(f_target).sum((1,2,3),keepdim=True)
 
-def style_mmd(f, f_target, kernel):
+    # reshape the feature maps into shape (batch_size x channels x M_l ), where M_l is height*width of our feature map
+    f, f_target = torch.flatten(f,start_dim=2,end_dim=3), torch.flatten(f_target,start_dim=2,end_dim=3)
 
-    # The number of rows we are going to sample
-    sample_size = f.size(1)
-    
-    # indexing tensor for sampling feature columns of size (sample_size, )
-    random_indices_f = torch.randint(low=0,high=sample_size,size=sample_size)
-    random_indices_f_target = torch.randint(low=0,high=sample_size,size=sample_size)
+    G = torch.bmm(f, torch.transpose(f,1,2))
+
+    #G.div(f.size(0)*f.size(1))
+
+    A = torch.bmm(f_target, torch.transpose(f_target,1,2))
+    #A.div(f_target.size(0)*f_target.size(1))
+    return ((G - A)**2).mean((1,2))
 
 
 def style_mmd_polynomial(f, f_target):
@@ -42,14 +46,39 @@ def style_mmd_polynomial(f, f_target):
     Computes the style loss proposed by Gatyes et al. (2016) by directly minimizing the Maximum Mean Discrepancy
     between the feature distributions using a polynomial kernel(Li et al. (2017))
     Args:
-        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, width, height, channel)
-        - f_target <torch.Tensor>: feature tensor of the style image with shape (batch_size, width, height, channel)
+        - f <torch.Tensor>: feature tensor of the image we are optimizing of shape (batch_size, channels, width, height)
+        - f_target <torch.Tensor>: feature tensor of the style image with shape (batch_size, channels, width, height)
     """
 
-    # reshape the feature maps into shape (batch_size x M_l x channels), where M_l is height*width of our feature map
-    f, f_target = torch.flatten(f,start_dim=1,end_dim=2), torch.flatten(f_target,start_dim=1,end_dim=2)
+    # reshape the feature maps into shape (batch_size x channels x M_l ), where M_l is height*width of our feature map
+    f, f_target = torch.flatten(f,start_dim=2,end_dim=3), torch.flatten(f_target,start_dim=2,end_dim=3)
 
     # The number of rows we are going to sample
-    sample_size = f.size(1)*f.size(2)
+    sample_size = 1024*8
+    channels = f.size(1)
+    batch_size = f.size(0)
 
+    # indexing tensor for sampling feature columns of size (batch_size x channels x sample_size)
+    random_indices_f = torch.randint(low=0,high=f.size(2),size=(sample_size,)).to(f.device).unsqueeze(0).unsqueeze(0).repeat(batch_size,channels,1)
+    random_indices_f_target = torch.randint(low=0,high=f.size(2),size=(sample_size,)).to(f.device).unsqueeze(0).unsqueeze(0).repeat(batch_size,channels,1)
+
+    # randomly sampled column vectors (batch_size x sample_size x channels)
+    random_columns_f = torch.gather(f,-1,random_indices_f).permute(0,2,1)
+    random_columns_f_target = torch.gather(f_target,-1,random_indices_f_target).permute(0,2,1)
+
+    pol_f = torch.bmm(random_columns_f,torch.transpose(random_columns_f,1,2))**2
+    pol_s = torch.bmm(random_columns_f_target,torch.transpose(random_columns_f_target,1,2))**2
+    pol_f_s = torch.bmm(random_columns_f,torch.transpose(random_columns_f_target,1,2))**2
+
+    loss = (pol_f + pol_s - 2*pol_f_s).sum((1,2))/(4*sample_size**2 * channels**2)
+
+    return loss
+
+def adaIN(f, f_target): 
+    mean = f.mean((2,3))
+    mean_target = f_target.mean((2,3))
+    std = torch.std(f,dim=(2,3))
+    std_target = torch.std(f_target,dim=(2,3))
+
+    return ((mean - mean_target)**2 + (std - std_target)**2).sum(1)/f.size(1)
     
--- utils/model.py
+++ utils/model.py
@@ -26,10 +26,53 @@ class Parallel(nn.Module):
 
         return (img,content_list,style_list)
 
-def construct_style_model(model, content_layers, style_layers):
-    #Normalize(mean = (0.485, 0.456, 0.406),std=(0.229, 0.224, 0.225))
+def construct_style_loss_model(model, content_layers, style_layers):
+
     seq = nn.Sequential()
+    print(range(max(content_layers+style_layers)+1))
     for i in range(max(content_layers+style_layers)+1):
         layer = model[i]
-        seq.add_module(Parallel(layer,i in content_layers, i in style_layers))
+        seq.add_module("Model layer: " + str(i) + " | Content layer: " + str(i in content_layers) + " | Style layer: " + str(i in style_layers),Parallel(layer,i in content_layers, i in style_layers))
     return seq
+
+
+class Deconv(nn.Module):
+    def __init__(self, layer, target_shape) -> None:
+        super().__init__()
+        self.layer = layer
+        self.target_shape = target_shape
+
+    def forward(self, x):
+        x = self.layer(x)
+        if not x.size() == self.target_shape:
+            x = F.interpolate(x,self.target_shape, mode = "bilinear")
+
+        return x
+    
+
+def construct_decoder_from_encoder(model, channels, input_width, input_height):
+    example_input = torch.rand((1,channels,input_width,input_height))
+    decoder = []
+
+    for i in range(len(model)):
+        layer = model[i]
+        example_output = layer(example_input)
+
+        if isinstance(layer, nn.Conv2d):
+            conv2d_transpose = nn.ConvTranspose2d(out_channels=example_input.size(1),in_channels=example_output.size(1),kernel_size=layer.kernel_size,stride=layer.stride)
+            decoder.append(Deconv(conv2d_transpose,example_input.size()[2:]))
+            decoder.append(nn.ReLU())
+        if isinstance(layer,nn.AvgPool2d) or isinstance(layer,nn.MaxPool2d):
+            pass
+
+        example_input = example_output
+
+    # remove last activation function
+    decoder = decoder[:-1]
+
+    # add sigmoid as last activation function
+    decoder.insert(0,nn.Sigmoid())
+
+    # Reverse list since we want to have the reverse order than the encoder
+    return nn.Sequential(*reversed(decoder))
+
